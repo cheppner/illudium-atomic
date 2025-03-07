8c7f2d5a72be52c8d53dcddb9002d887c0556a41
cheppner <charles@heppner.io>
2025-03-07 14:36:33 +0100

    added -y flag to all dnf commands

--- build.sh
+++ build.sh
@@ -14,13 +14,13 @@ set -ouex pipefail
 
 dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
 dnf install -y https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
-dnf config-manager setopt fedora-cisco-openh264.enabled=1
+dnf -y config-manager setopt fedora-cisco-openh264.enabled=1
 dnf update -y
-dnf swap ffmpeg-free ffmpeg --allowerasing
-dnf swap mesa-va-drivers mesa-va-drivers-freeworld
-dnf swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld
-dnf swap mesa-va-drivers.i686 mesa-va-drivers-freeworld.i686
-dnf swap mesa-vdpau-drivers.i686 mesa-vdpau-drivers-freeworld.i686
+dnf -y swap ffmpeg-free ffmpeg --allowerasing
+dnf -y swap mesa-va-drivers mesa-va-drivers-freeworld
+dnf -y swap mesa-vdpau-drivers mesa-vdpau-drivers-freeworld
+dnf -y swap mesa-va-drivers.i686 mesa-va-drivers-freeworld.i686
+dnf -y swap mesa-vdpau-drivers.i686 mesa-vdpau-drivers-freeworld.i686
 #dnf install -y rpmfusion-nonfree-release-tainted
 # todo - add VSCODE
 dnf install -y tmux 
