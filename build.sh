72fa5ed3197355e1ccb87cad6e974cbd3f960961
cheppner <charles@heppner.io>
2025-03-08 05:00:01 +0100

    disable vscode install for now

--- build.sh
+++ build.sh
@@ -55,7 +55,7 @@ dnf -y copr disable lizardbyte/stable
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | tee /etc/yum.repos.d/vscode.repo > /dev/null
 dnf -y check-update
-dnf -y install code
+#dnf -y install code
 
 # Use a COPR Example:
 #
