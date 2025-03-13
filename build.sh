f895696bfbc6c4e203fb5b90b8a94beff7daed24
cheppner <charles@heppner.io>
2025-03-13 12:52:09 +0100

    cleanup, added new packages and flathub

--- build.sh
+++ build.sh
@@ -15,12 +15,12 @@ set -ouex pipefail
 setsebool -P domain_kernel_load_modules on
 
 # install cachyos kernel
-dnf -y copr enable bieszczaders/kernel-cachyos
-dnf -y copr enable bieszczaders/kernel-cachyos-addons
-dnf -y install kernel-cachyos kernel-cachyos-devel-matched
-dnf -y install scx-scheds
-dnf -y copr disable bieszczaders/kernel-cachyos-addons
-dnf -y copr disable bieszczaders/kernel-cachyos
+# dnf -y copr enable bieszczaders/kernel-cachyos
+# dnf -y copr enable bieszczaders/kernel-cachyos-addons
+# dnf -y install kernel-cachyos kernel-cachyos-devel-matched
+# dnf -y install scx-scheds
+# dnf -y copr disable bieszczaders/kernel-cachyos-addons
+# dnf -y copr disable bieszczaders/kernel-cachyos
 
 # dedupe
 
@@ -36,26 +36,56 @@ dnf -y swap mesa-va-drivers.i686 mesa-va-drivers-freeworld.i686
 dnf -y swap mesa-vdpau-drivers.i686 mesa-vdpau-drivers-freeworld.i686
 #dnf install -y rpmfusion-nonfree-release-tainted
 # todo - add VSCODE
-dnf install -y tmux 
-dnf install -y steam
-dnf install -y gamescope
-dnf install -y mangohud
-dnf install -y goverlay
-dnf install -y btop
-dnf install -y rocm-smi
-dnf install -y google-noto-* --allowerasing --skip-broken
-dnf -y copr enable ilyaz/LACT
-dnf -y copr enable lizardbyte/stable
-dnf install -y lact
-dnf install -y Sunshine
-dnf -y copr disable ilyaz/LACT
-dnf -y copr disable lizardbyte/stable
+
+sudo dnf5 -y install \
+                    firefox \
+                    steam \
+                    gamescope \
+                    mangohud \
+                    goverlay \
+                    btop \
+                    rocm-smi \
+                    source-foundry-hack-fonts \
+                    kate \
+                    vlc \
+                    krita \
+                    fastfetch \
+                    tmux \
+                    unifont-fonts
 
 # VSCode
 rpm --import https://packages.microsoft.com/keys/microsoft.asc
 echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\nautorefresh=1\ntype=rpm-md\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" | tee /etc/yum.repos.d/vscode.repo > /dev/null
 dnf -y check-update
 #dnf -y install code
+
+# install packages from Nobara COPR
+sudo dnf5 -y copr enable gloriouseggroll/nobara-41
+sudo dnf5 -y install umu-launcher \
+                     starship
+sudo dnf5 -y copr disable gloriouseggroll/nobara-41
+
+# Install and enable System76 scheduler
+sudo dnf5 -y copr enable kylegospo/system76-scheduler
+sudo dnf5 -y install system76-scheduler
+sudo systemctl enable com.system76.Scheduler.service
+sudo dnf5 -y copr disable kylegospo/system76-scheduler
+
+# Install sunshine
+sudo dnf5 -y copr enable lizardbyte/stable
+sudo dnf5 -y install Sunshine
+
+# Install and enable LACT
+sudo dnf5 -y copr enable ilyaz/LACT
+sudo dnf5 -y install lact
+sudo systemctl enable lactd
+sudo dnf5 -y copr disable ilyaz/LACT
+
+# Enable flatpak repo
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
+
+# Install flatpaks
+flatpak install -y --noninteractive flathub net.davidotek.pupgui2
 
 # Use a COPR Example:
 #
